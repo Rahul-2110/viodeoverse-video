25bf364fe018b7530617ae477000562bda797de0
Rahul <rahultambe@gofynd.com>
2024-12-06 01:12:12 +0530

    upload videos validations

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,10 +1,10 @@
 const { Router } = require('express');
-const { validateRequest } = require('../utils/validators/request.validator');
 const { uploadVideo } = require('../controllers/video.controller');
+const { uploadValidator, validateVideoDuration } = require('../utils/video');
 
 const router = Router();
 
-router.post('/upload', uploadVideo);
+router.post('/upload', [uploadValidator], uploadVideo);
 
 
 module.exports = router;
