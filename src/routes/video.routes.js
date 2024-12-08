98caefa0b7b6bd1c6cb25c0b178cdfc502384cb4
Rahul-2110 <iamrahultambe@gmail.com>
2024-12-08 17:10:09 +0530

    Cut and Merge videos

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,11 +1,17 @@
 const { Router } = require('express');
-const { uploadVideo } = require('../controllers/video.controller');
+const { uploadVideo, trimVideo, mergeVideo } = require('../controllers/video.controller');
 const { uploadValidator } = require('../utils/video');
 const authMiddleware = require('../middlewares/auth');
+const { validateRequest } = require('../utils/validators/request.validator');
+const { trimVideoSchema, mergegeVideoSchema } = require('../utils/validators/video.validator');
 
 const router = Router();
 
-router.post('/upload', [authMiddleware, uploadValidator], uploadVideo);
+router.use(authMiddleware);
+
+router.post('/upload', uploadValidator, uploadVideo);
+router.post('/trim/:id', validateRequest(trimVideoSchema), trimVideo);
+router.post('/merge', validateRequest(mergegeVideoSchema), mergeVideo);
 
 
 module.exports = router;
